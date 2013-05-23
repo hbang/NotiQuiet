814e83afa7e143acaa8fc48bc33bd7420663fee7
Adam D <kirbylover4000@gmail.com>
2013-05-23 12:49:47 +0930

    cleanup ADNQListController

--- prefs/ADNQListController.m
+++ prefs/ADNQListController.m
@@ -1,19 +1,22 @@
-#import <Preferences/Preferences.h>
-@interface ADNQListController:PSListController{}
-@end
+#import <Preferences/PSListController.h>
+
 @implementation ADNQListController
--(id)specifiers{
-	if(_specifiers==nil)_specifiers=[[self loadSpecifiersFromPlistName:@"NotiQuiet" target:self]retain];
+- (id)specifiers {
+	if (!_specifiers) {
+		_specifiers = [[self loadSpecifiersFromPlistName:@"NotiQuiet" target:self] retain];
+	}
+	
 	return _specifiers;
 }
--(void)adtwitter:(id)params{
-	if([[UIApplication sharedApplication]canOpenURL:[NSURL URLWithString:@"tweetbot:"]]){
+
+- (void)followOnTwitter {
+	if ([[UIApplication sharedApplication]canOpenURL:[NSURL URLWithString:@"tweetbot:"]]) {
 		[[UIApplication sharedApplication]openURL:[NSURL URLWithString:@"tweetbot:///user_profile/thekirbylover"]];
-	}else if([[UIApplication sharedApplication]canOpenURL:[NSURL URLWithString:@"tweetings:"]]){
+	} else if ([[UIApplication sharedApplication]canOpenURL:[NSURL URLWithString:@"tweetings:"]]) {
 		[[UIApplication sharedApplication]openURL:[NSURL URLWithString:@"tweetings:///user?screen_name=thekirbylover"]];
-	}else if([[UIApplication sharedApplication]canOpenURL:[NSURL URLWithString:@"twitter:"]]){
+	} else if ([[UIApplication sharedApplication]canOpenURL:[NSURL URLWithString:@"twitter:"]]) {
 		[[UIApplication sharedApplication]openURL:[NSURL URLWithString:@"twitter://user?screen_name=thekirbylover"]];
-	}else{
+	} else {
 		[[UIApplication sharedApplication]openURL:[NSURL URLWithString:@"http://twitter.com/intent/follow?screen_name=thekirbylover"]];
 	}
 }
