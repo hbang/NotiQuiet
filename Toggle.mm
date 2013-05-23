c115aecd70fa5d55092dbceb9f90ac8f80d56c76
Adam D <kirbylover4000@gmail.com>
2013-05-23 12:47:31 +0930

    update toggle.mm to follow the coding style
    (did we ever release this?!)

--- Toggle.mm
+++ Toggle.mm
@@ -1,18 +1,24 @@
 #include <notify.h>
-#define prefpath @"/var/mobile/Library/Preferences/ws.hbang.notiquiet.plist"
-extern "C" BOOL isCapable(){
+
+#define kADNQPrefsPath @"/var/mobile/Library/Preferences/ws.hbang.notiquiet.plist"
+
+extern "C" BOOL isCapable() {
 	return YES;
 }
-extern "C" BOOL isEnabled(){
-	NSDictionary *prefs=[[NSDictionary alloc]initWithContentsOfFile:prefpath];
-	return !prefs||![prefs objectForKey:@"Enabled"]||![[prefs objectForKey:@"Enabled"]boolValue];
+
+extern "C" BOOL isEnabled() {
+	NSDictionary *prefs = [NSDictionary dictionaryWithContentsOfFile:kADNQPrefsPath];
+	return [prefs objectForKey:@"Enabled"] ? ![prefs objectForKey:@"Enabled"] boolValue] : YES;
 }
-extern "C" void setState(BOOL enabled){
-	NSMutableDictionary *prefs=[[NSMutableDictionary alloc]initWithContentsOfFile:prefpath];
-	[prefs setObject:[NSNumber numberWithBool:!(!prefs||![prefs objectForKey:@"Enabled"]||[[prefs objectForKey:@"Enabled"]boolValue])] forKey:@"Enabled"];
+
+extern "C" void setState(BOOL enabled) {
+	NSMutableDictionary *prefs = [NSMutableDictionary dictionaryWithContentsOfFile:kADNQPrefsPath];
+	[prefs setObject:[NSNumber numberWithBool:[prefs objectForKey:@"Enabled"] ? ![prefs objectForKey:@"Enabled"] : YES] forKey:@"Enabled"];
 	[prefs writeToFile:prefpath atomically:YES];
+	
 	notify_post("ws.hbang.notiquiet/ReloadPrefs");
 }
-extern "C" float getDelayTime(){
+
+extern "C" float getDelayTime() {
 	return 0.1f;
 }
